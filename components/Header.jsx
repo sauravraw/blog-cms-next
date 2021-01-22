54f0be40aae2d5f827efda626c0cc04de63f8ba8
saurav <sourav.upadhyay@raweng.com>
2021-01-22 05:46:55 +0530

    header added sucessfully

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
 
 function Header(props) {
 	let header = props.header[0];
 	console.log(props.header[0]);
 	return (
-		<div>
-			<h1>Saurav</h1>
+		<div className={styles.navContainer}>
+			<div className={styles.navHeader}>
+				<img
+					className={styles.navImage}
+					src={header.header_image.blog_header_image.url}
+					alt={header.header_image.blog_header_title}
+				/>
+				<h1>{header.header_image.blog_header_title}</h1>
+			</div>
+			<div className={styles.navLinkContainer}>
+				<p className={styles.navLink} key={header.uid}>
+					{header.navlink.map((link) => {
+						return (
+							<Link href={link.href}>
+								<a>{link.title}</a>
+							</Link>
+						);
+					})}
+				</p>
+			</div>
 		</div>
 	);
 }
